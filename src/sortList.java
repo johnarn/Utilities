dc56ff61753cfc5dd8b5a9bf6589731d4ee97cc2
= <johnarn@windowslive.com>
2019-05-31 17:48:29 +0300

    Utilities source files

--- src/sortList.java
+++ src/sortList.java
@@ -43,4 +43,44 @@ public class sortList {
         }
         return list;
     }
+
+    public static List<Double> sortListOfDouble(List<Double> list) {
+        if (list == null) {
+            return null;
+        }
+        if (list.size() == 0) {
+            return new ArrayList<>();
+        }
+        Double temp;
+        for (int i = 0; i < list.size() - 1; i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                if (list.get(i).compareTo(list.get(j)) > 0) {
+                    temp = list.get(i);
+                    list.set(i, list.get(j));
+                    list.set(j, temp);
+                }
+            }
+        }
+        return list;
+    }
+
+    public static List<Float> sortListOfFloat(List<Float> list) {
+        if (list == null) {
+            return null;
+        }
+        if (list.size() == 0) {
+            return new ArrayList<>();
+        }
+        float temp;
+        for (int i = 0; i < list.size() - 1; i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                if (list.get(i).compareTo(list.get(j)) > 0) {
+                    temp = list.get(i);
+                    list.set(i, list.get(j));
+                    list.set(j, temp);
+                }
+            }
+        }
+        return list;
+    }
 }
