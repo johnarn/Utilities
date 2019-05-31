68d7fd980419454ca6da8419bcfcf64b240b6eda
= <johnarn@windowslive.com>
2019-05-31 17:13:18 +0300

    Added String sort

--- src/sortList.java
+++ src/sortList.java
@@ -23,4 +23,24 @@ public class sortList {
         }
         return list;
     }
+
+    public static List<String> sortListOfStrings(List<String> list) {
+        if (list == null) {
+            return null;
+        }
+        if (list.size() == 0) {
+            return new ArrayList<>();
+        }
+        String temp;
+        for (int i = 0; i < list.size() - 1; i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                if (list.get(i).compareTo(list.get(j)) > 0) {
+                    temp = list.get(i);
+                    list.set(i, list.get(j));
+                    list.set(j, temp);
+                }
+            }
+        }
+        return list;
+    }
 }
