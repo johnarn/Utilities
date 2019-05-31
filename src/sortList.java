1cfcf225c76d2074e9877261866a1d5e40458f97
= <johnarn@windowslive.com>
2019-05-31 16:58:36 +0300

    Utilities source file

--- src/sortList.java
+++ src/sortList.java
@@ -4,17 +4,17 @@ import java.util.List;
 public class sortList {
 
 
-    public static List<Integer> sortListOfIntegers(List<Integer> list){
+    public static List<Integer> sortListOfIntegers(List<Integer> list) {
         if (list == null) {
             return null;
         }
-        if(list.size() == 0){
+        if (list.size() == 0) {
             return new ArrayList<>();
         }
         int temp;
-        for(int i=0; i<list.size()-1; i++){
-            for(int j=i+1; j<list.size(); j++){
-                if(list.get(i).compareTo(list.get(j)) > 0){
+        for (int i = 0; i < list.size() - 1; i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                if (list.get(i).compareTo(list.get(j)) > 0) {
                     temp = list.get(i);
                     list.set(i, list.get(j));
                     list.set(j, temp);
