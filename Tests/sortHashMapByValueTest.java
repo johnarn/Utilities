c5dbbd9cf6b8b772540cdfddf2e2ef2f6b4d7010
= <johnarn@windowslive.com>
2019-06-21 00:47:15 +0300

    Test of Sort HashMap By Value code

--- Tests/sortHashMapByValueTest.java
+++ Tests/sortHashMapByValueTest.java
@@ -51,11 +51,7 @@ public class sortHashMapByValueTest {
         sortedHashMap.put("second", -14.0);
         sortedHashMap.put("first", -10.0);
 
-
-
         Map<String, Double> newSortedHashmap = sortHashMapByValue.sortByValue(hashmap);
-
-
 
         List newsortedkeys = new ArrayList(newSortedHashmap.keySet());
         List sortedkeys = new ArrayList(sortedHashMap.keySet());
@@ -71,6 +67,35 @@ public class sortHashMapByValueTest {
         Map<String, Double> emptyHashMap = new HashMap<>();
         assertEquals(emptyHashMap, newSortedHashmap);
     }
+
+
+    @Test
+    public void testSortByValueWithStringAsKeyAndMixDoubleAsValue() {
+        Map<String, Double> hashmap = new HashMap<>();
+
+        hashmap.put("first", -10.0);
+        hashmap.put("third", 0.0);
+        hashmap.put("second", 14.0);
+        hashmap.put("fifth", 50.0);
+        hashmap.put("fourth", -35.0);
+
+        Map<String, Double> sortedHashMap = new LinkedHashMap<>();
+        sortedHashMap.put("fourth", -35.0);
+        sortedHashMap.put("first", -10.0);
+        sortedHashMap.put("third", 0.0);
+        sortedHashMap.put("second", 14.0);
+        sortedHashMap.put("fifth", 50.0);
+
+        Map<String, Double> newSortedHashmap = sortHashMapByValue.sortByValue(hashmap);
+
+        List newsortedkeys = new ArrayList(newSortedHashmap.keySet());
+        List sortedkeys = new ArrayList(sortedHashMap.keySet());
+
+        for(int i=0; i<sortedkeys.size(); i++){
+            assertEquals(newsortedkeys.get(i), sortedkeys.get(i));
+        }
+    }
+
 
 
 }
